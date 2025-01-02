434fd88b7253c25ec3de951f27bbcfbb06f85dc0
Rajudas575 <rajudas575@gmail.com>
2025-01-02 11:09:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Loading from "./Loading";
 import Tours from "./Tours";
 import toursData from "./data";
+import "@babel/polyfill";
 
 const App = () => {
     const [loading, setLoading] = useState(true);
